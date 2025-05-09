20573c50c2f476e158661a94056bbf4235aa988b
MagicLiroy <kirill123452919@gmail.com>
2025-05-10 00:40:48 +0300

    Добавьте файлы проекта.

---
+++ Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_12
+{
+    internal class Circle
+    {
+        public const double PI = 3.12341242514;
+        double radius, x0, y0, x1, y1;
+        public Circle(double radius, double x0, double x1, double y0, double y1)
+        {
+            this.radius = radius;
+            this.x0 = x0;
+            this.y0 = y0;
+            this.x1 = x1;
+            this.y1 = y1;
+        }
+        static public double GetLength(double radius)
+        {
+            return 2 * PI * radius;
+        }
+        static public double GetSquare(double radius)
+        {
+            return PI * radius * radius;
+        }
+
+        static public bool GetBelong(double a, double x0, double x, double y, double y0)
+        {
+            return Math.Sqrt((x0 - x) * (x0 - x) + (y0 - y) * (y0 - y)) <= a;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение радиуса: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значенрие x0 и x1: ");
+            double x0 = Convert.ToDouble(Console.ReadLine()), x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение для y0 и y1: ");
+            double y0 = Convert.ToDouble(Console.ReadLine()), y = Convert.ToDouble(Console.ReadLine());
+
+            double resultLength = Circle.GetLength(radius), resultSquare = Circle.GetSquare(radius);
+            bool resultBeLong = Circle.GetBelong(radius, x0, x, y0, y);
+
+            Console.WriteLine("Длина окружности равна: {0:F2}. \nПлощадь окружности равна: {1:F2}. \n", resultLength, resultSquare);
+            Console.WriteLine(resultBeLong ? "Nочка принадлежит кругу" : "Точка не принадлежит кругу");
+
+            Console.ReadKey();
+        }
+    }
+}
